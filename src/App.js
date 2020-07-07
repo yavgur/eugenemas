e06f67d5d738e53849401e1280799a0cd1334c28
cpasternak <cpasternak@cbtnuggets.com>
2020-07-07 16:04:53 -0700

    fixed routing for the why islam page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import Header from './Components/Header'
-import Prayertime from './Components/Prayertime'
-import PrayerTable from './Components/PrayerTable'
-import Footer from './Components/Footer'
-import Tevhid from './Components/Tevhid'
-import Ourmasjid from './Components/Ourmasjid'
-import Madina from './Components/Madina'
-import Donate from './Components/Donate'
-import WhyIslam from './Components/WhyIslam'
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import Prayertime from './Components/Prayertime';
+import PrayerTable from './Components/PrayerTable';
+import Footer from './Components/Footer';
+import Tevhid from './Components/Tevhid';
+import Ourmasjid from './Components/Ourmasjid';
+import Madina from './Components/Madina';
+import Donate from './Components/Donate';
+import WhyIslam from './Components/WhyIslam';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -15,40 +15,23 @@ class App extends Component {
     return (
       <BrowserRouter>     
         <div>
-          <Header />
-          <Prayertime />
-          <PrayerTable />
-          <Tevhid />
-          <Ourmasjid />
-          <Madina />
-          <Donate />
           <Switch>
-          <Route exact path="/whyislam" component={WhyIslam}/>
-          <Footer />
-          </Switch>
+          <Route exact path="/">
+            <Header />
+            <Prayertime />
+            <PrayerTable />
+            <Tevhid />
+            <Ourmasjid />
+            <Madina />
+            <Donate />
+            <Footer />
+          </Route>  
+          <Route path="/whyislam" exact component={WhyIslam}/>          
+          </Switch>          
         </div>
-        </BrowserRouter>
-      
+      </BrowserRouter>      
     )
   }
 }
 
-export default App
-
-/*
-   <div>
-          <Route component={Header} />
-          <Route component={Prayertime} />
-          <Route component={PrayerTable} />
-          <Route component={Tevhid} />
-          <Route component={Ourmasjid} />
-          <Route component={Madina} />
-          <Route component={Donate} />
-          <Switch>
-          <Route path="/whyislam" render={() => (
-            <WhyIslam />
-          )}/>
-          </Switch>
-          <Footer />
-        </div>
-  */
+export default App;
